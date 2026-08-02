9b60f314deb7be7beacc0513aea82991d1d4c6ad
textbrowser@gmail.com <textbrowser@gmail.com>
2026-08-02 15:56:45 -0400

    Qt 5.7.

--- Source/spinware_main.cc
+++ Source/spinware_main.cc
@@ -33,7 +33,9 @@
 
 int main(int argc, char *argv[])
 {
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 7, 0))
   QCoreApplication::setAttribute(Qt::AA_DontUseNativeDialogs, true);
+#endif
 
   QApplication qapplication(argc, argv);
 
