70888657f5d4ff8312ed9c776a650250ba68dfd4
textbrowser@gmail.com <textbrowser@gmail.com>
2026-08-02 15:54:05 -0400

    Simple files.

--- spinware.sh
+++ spinware.sh
@@ -8,19 +8,21 @@ export QT_AUTO_SCREEN_SCALE_FACTOR=1
 
 export QT_X11_NO_MITSHM=1
 
-if [ -r ./spinware ] && [ -x ./spinware ]
+if [ -f ./spinware ] && [ -r ./spinware ] && [ -x ./spinware ]
 then
     echo "Launching a local Spinware."
     ./spinware
     exit $?
 fi
 
-if [ -r /opt/spinware/spinware ] && [ -x /opt/spinware/spinware ]
+if [ -f /opt/spinware/spinware ] && \
+   [ -r /opt/spinware/spinware ] && \
+   [ -x /opt/spinware/spinware ]
 then
-    echo "Launching an official Spinware."
+    echo "Launching an official Spinware (/opt/spinware)."
     cd /opt/spinware && ./spinware
     exit $?
-else
-    echo "Cannot locate Spinware."
-    exit 1
 fi
+
+echo "Cannot locate Spinware."
+exit 1
